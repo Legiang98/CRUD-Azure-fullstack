63bd7b7f9a796ec990bd7a72d76794e12294a303
Giang Le Vu <legiang98@gmail.com>
2025-10-31 13:17:11 +0700

    Add role assignments

--- infrastructure/lib/acr.ts
+++ infrastructure/lib/acr.ts
@@ -6,7 +6,16 @@ import {
     acrStack
 } from "../stacks/backend/acr";
 
+import { SystemAssignedIdentityPricipalId } from "./backend";
+
 const config = new pulumi.Config();
 const acrConfigParams = config.requireObject("acr") as ACRConfig;
 
 export const devAcr = new acrStack("dev-acr-stack", acrConfigParams);
+
+const roleAssignment = new azure_native.authorization.RoleAssignment("acr-role-assignment", {
+    principalId: SystemAssignedIdentityPricipalId,
+    principalType: "ServicePrincipal",
+    roleDefinitionId: "/providers/Microsoft.Authorization/roleDefinitions/7f951dda-4ed3-4680-a7ca-43fe172d538d", // AcrPull
+    scope: devAcr.registryId,
+});
